65bec9b8b3dfde53f142f952b70b5587ce4466b8
GaliardArt <tijolinhodeveludo@gmail.com>
2025-05-23 12:22:59 -0300

    Paddle Movment, and Input Config

--- Scenes/Paddle/Paddle.gd
+++ Scenes/Paddle/Paddle.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 
+const SPEED: float = 200
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +10,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	#if Input.is_action_pressed("move_left") == true :
+	#	position.x -= SPEED * delta
+	#if Input.is_action_pressed("move_right") == true:
+	#	position.x += SPEED * delta
+		
+	var movement: float = Input.get_axis("move_left", "move_right")
+	position.x += SPEED * delta * movement
+	position.x = clampf(position.x, 45, get_viewport_rect().end.x-45)
