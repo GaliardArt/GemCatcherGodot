7b4261f0c8b1f42c16461a6e0c7c5da8a4cdf8d0
GaliardArt <tijolinhodeveludo@gmail.com>
2025-05-30 20:43:44 -0300

    Signals Test, and destroy gem with collision

--- Scenes/Gem/Gem.gd
+++ Scenes/Gem/Gem.gd
@@ -6,11 +6,16 @@ const SPEED: float = 100
 func _ready() -> void:
 	pass # Replace with function body.
 
-
+func die() -> void:
+	set_process(false)
+	queue_free()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.y += SPEED * delta
 	if position.y > get_viewport_rect().end.y :
 		print("Gem falls off")
-		set_process(false)
-		queue_free()
+		die()
+
+func _on_area_entered(area: Area2D) -> void:
+	print("Gem Hits Paddle")
+	die()
--- Scenes/Paddle/Paddle.gd
+++ Scenes/Paddle/Paddle.gd
@@ -18,3 +18,7 @@ func _process(delta: float) -> void:
 	var movement: float = Input.get_axis("move_left", "move_right")
 	position.x += SPEED * delta * movement
 	position.x = clampf(position.x, 45, get_viewport_rect().end.x-45)
+
+
+func _on_area_entered(area: Area2D) -> void:
+	print("Collide", area)
